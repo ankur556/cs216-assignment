5abe6cef41f7437fcae073c6362fa5a624648459
saransh <saransh2304@gmail.com>
2026-01-31 19:31:59 +0530

    pre-anurag

--- src/transactions.py
+++ src/transactions.py
@@ -1,7 +1,19 @@
 from dataclasses import dataclass
 
 @dataclass
+class TransactionInput:
+    prev_tx: str
+    index: int
+    owner: str
+
+@dataclass
 class TransactionOutput:
     amount: float
     address: str
+
+class Transaction:
+    def __init__(self, tx_id: str, inputs: list[TransactionInput], outputs: list[TransactionOutput]):
+        self.tx_id = tx_id
+        self.inputs = inputs  # List of (tx_id, index) tuples
+        self.outputs = outputs  # List of TransactionOutput instances
 
